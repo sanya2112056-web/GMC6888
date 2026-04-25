42b53eb479889ad723db6fd1615b52329e7b6f76
sanya2112056-web <sanya2112056@gmail.com>
2026-04-25 21:00:54 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .crypto import CryptoScanner
-from .freelance import FreelanceScanner
-from .market_pulse import MarketPulseScanner
-
-__all__ = ["CryptoScanner", "FreelanceScanner", "MarketPulseScanner"]
