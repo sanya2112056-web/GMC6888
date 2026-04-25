647cb4b7603f9f5d6c2e4df0d3ca925e201dca24
sanya2112056-web <sanya2112056@gmail.com>
2026-04-25 20:35:36 +0200

    Add files via upload

---
+++ scanners/__init__.py
@@ -0,0 +1,5 @@
+from .crypto import CryptoScanner
+from .freelance import FreelanceScanner
+from .market_pulse import MarketPulseScanner
+
+__all__ = ["CryptoScanner", "FreelanceScanner", "MarketPulseScanner"]
